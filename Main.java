c64d2ab69169f6e2736d8732ba3b6894c7432713
sandg9896 <sandg9896@wrdsb.ca>
2020-10-08 14:49:56 +0000

    Locate Same Number

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * state any index that the number is located at
+ * @ Gavin 
  */
 public class Main {
 
@@ -9,7 +10,34 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Create scanner for user input
+    Scanner input = new Scanner(System.in);
     
+    // create array with 10 spots
+    int[] integers = new int[10];
+
+    // ask user to enter ten numbers
+    System.out.println("Please enter in 10 integers to put into the array");
+
+    // put integers into array using loop
+    for(int i = 0; i < integers.length; i++){
+     integers[i] = input.nextInt();
+    }
+  
+    // ask user to enter a number to find within array
+    System.out.println("Please enter a number to find");
+    int findNumber = input.nextInt();
+
+    // create loop to find number chosen
+    for(int i = 0; i < integers.length; i++){
+      if(integers[i] == findNumber)
+      System.out.println(findNumber + " is located at index " + i);
+    }
+
+
   }
 }
+
+    
+  
+
